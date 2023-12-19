f506c775028617282099092180325cc748b666b1
Grigoriy <grigoriymikhalchuk@yahoo.com>
2023-12-19 17:07:05 +0100

    decouple the local datastore

--- data/local-datastorage-impl/src/main/java/com/grappim/hateitorrateit/data/localdatastorageimpl/LocalDataStorageImpl.kt
+++ data/local-datastorage-impl/src/main/java/com/grappim/hateitorrateit/data/localdatastorageimpl/LocalDataStorageImpl.kt
@@ -1,15 +1,12 @@
 package com.grappim.hateitorrateit.data.localdatastorageimpl
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.grappim.hateitorrateit.data.localdatastorageapi.LocalDataStorage
 import com.grappim.hateitorrateit.domain.HateRateType
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
@@ -18,32 +15,30 @@ import javax.inject.Singleton
 
 @Singleton
 class LocalDataStorageImpl @Inject constructor(
-    @ApplicationContext private val context: Context,
+    private val dataStore: DataStore<Preferences>,
 ) : LocalDataStorage {
 
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "hateitratestore")
-
     private val TYPE_KEY = stringPreferencesKey("type_key")
-    override val typeFlow: Flow<HateRateType> = context.dataStore.data
+    override val typeFlow: Flow<HateRateType> = dataStore.data
         .map { preferences ->
             enumValueOf(preferences[TYPE_KEY] ?: HateRateType.HATE.name)
         }
 
     private val CRASHES_KEY = booleanPreferencesKey("crashes_key")
-    override val crashesCollectionEnabled: Flow<Boolean> = context.dataStore.data
-            .map { preferences ->
-                preferences[CRASHES_KEY] ?: true
-            }
+    override val crashesCollectionEnabled: Flow<Boolean> = dataStore.data
+        .map { preferences ->
+            preferences[CRASHES_KEY] ?: true
+        }
 
     override suspend fun setCrashesCollectionEnabled(isEnabled: Boolean) {
         Timber.d("setCrashesCollectionEnabled: $isEnabled")
-        context.dataStore.edit { settings ->
+        dataStore.edit { settings ->
             settings[CRASHES_KEY] = isEnabled
         }
     }
 
     override suspend fun changeTypeTo(type: HateRateType) {
-        context.dataStore.edit { settings ->
+        dataStore.edit { settings ->
             settings[TYPE_KEY] = type.name
         }
     }
---
+++ data/local-datastorage-impl/src/main/java/com/grappim/hateitorrateit/data/localdatastorageimpl/LocalDataStorageModule.kt
@@ -0,0 +1,25 @@
+package com.grappim.hateitorrateit.data.localdatastorageimpl
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@[Module InstallIn(SingletonComponent::class)]
+class LocalDataStorageModule {
+
+    @Provides
+    fun provideDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
+        produceFile = {
+            context.preferencesDataStoreFile("hateitratestore")
+        }
+    )
+}
