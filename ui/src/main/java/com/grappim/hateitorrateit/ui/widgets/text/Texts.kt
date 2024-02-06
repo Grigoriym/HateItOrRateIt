bc50798ecac4b603a1864b781585f9830bd8d34e
Grigoriy <grigoriymikhalchuk@yahoo.com>
2024-02-06 22:37:16 +0100

    Fix name text incorrectly shown in details

--- ui/src/main/java/com/grappim/hateitorrateit/ui/widgets/text/Texts.kt
+++ ui/src/main/java/com/grappim/hateitorrateit/ui/widgets/text/Texts.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import com.grappim.hateitorrateit.ui.theme.HateItOrRateItTheme
 import com.grappim.hateitorrateit.ui.utils.ThemePreviews
@@ -21,6 +22,7 @@ fun TextH4(
         style = MaterialTheme.typography.h4,
         overflow = overflow,
         maxLines = maxLines,
+        textAlign = TextAlign.Center
     )
 }
 
