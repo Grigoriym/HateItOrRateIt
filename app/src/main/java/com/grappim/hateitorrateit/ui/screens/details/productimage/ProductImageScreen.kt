f7bf97b6e604b9b24181ff03b6f38a3266f17f7b
Mikhalchuk Grigoriy <31949421+Grigoriym@users.noreply.github.com>
2024-03-21 20:41:31 +0100

    Add zoom in image details (#74)

--- app/src/main/java/com/grappim/hateitorrateit/ui/screens/details/productimage/ProductImageScreen.kt
+++ app/src/main/java/com/grappim/hateitorrateit/ui/screens/details/productimage/ProductImageScreen.kt
@@ -1,33 +1,42 @@
 package com.grappim.hateitorrateit.ui.screens.details.productimage
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
-import com.grappim.hateitorrateit.ui.widgets.PlatoPagerIndicator
 import com.grappim.hateitorrateit.ui.widgets.PlatoTopBar
+import kotlin.math.max
+
+private const val SCALE_COERCE_MAXIMUM_VALUE = 3f
 
 @Composable
 fun ProductImageScreen(viewModel: ProductImageViewModel = hiltViewModel(), goBack: () -> Unit) {
     val state by viewModel.viewState.collectAsStateWithLifecycle()
 
     if (state.uri.isNotEmpty()) {
-        ImageContent(
+        ProductImageContent(
             state = state,
             goBack = goBack
         )
@@ -35,37 +44,23 @@ fun ProductImageScreen(viewModel: ProductImageViewModel = hiltViewModel(), goBac
 }
 
 @Composable
-private fun ImageContent(state: ImageViewModelState, goBack: () -> Unit) {
-    val pagerState = rememberPagerState {
-        state.images.size
-    }
-
+private fun ProductImageContent(state: ImageViewModelState, goBack: () -> Unit) {
     Scaffold(
         modifier = Modifier
             .statusBarsPadding()
             .navigationBarsPadding()
     ) {
-        Box(
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-        ) {
-            HorizontalPager(
+        Box {
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .align(Alignment.Center),
-                state = pagerState
-            ) { page ->
-                val file = state.images[page]
-                Image(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    painter = rememberAsyncImagePainter(file.uriString),
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop
+                    .padding(it)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                ImageContent(
+                    uriString = state.uri
                 )
             }
-
             PlatoTopBar(
                 modifier = Modifier.padding(top = 2.dp),
                 goBack = goBack,
@@ -73,14 +68,52 @@ private fun ImageContent(state: ImageViewModelState, goBack: () -> Unit) {
                 backgroundColor = Color.Transparent,
                 elevation = 0.dp
             )
-
-            PlatoPagerIndicator(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter),
-                show = state.images.size > 1,
-                size = state.images.size,
-                pagerState = pagerState
-            )
         }
     }
+}
+
+@Composable
+private fun ImageContent(uriString: String) {
+    var scale by remember {
+        mutableFloatStateOf(1f)
+    }
+    var offset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    val painter = rememberAsyncImagePainter(uriString)
+
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, zoom, _ ->
+                    scale *= zoom
+                    scale = scale.coerceIn(1f, SCALE_COERCE_MAXIMUM_VALUE)
+
+                    val extraWidth = max(
+                        0f,
+                        (painter.intrinsicSize.width * scale - painter.intrinsicSize.width) / 2
+                    )
+                    val extraHeight = max(
+                        0f,
+                        (painter.intrinsicSize.height * scale - painter.intrinsicSize.height) / 2
+                    )
+
+                    offset = Offset(
+                        x = (offset.x + pan.x * scale).coerceIn(-extraWidth, extraWidth),
+                        y = (offset.y + pan.y * scale).coerceIn(-extraHeight, extraHeight)
+                    )
+                }
+            }
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+                translationX = offset.x
+                translationY = offset.y
+            },
+        painter = painter,
+        contentDescription = "",
+        contentScale = ContentScale.Crop
+    )
 }
