2229f428964560a797f16f8176af9f19e4af9ba9
Mikhalchuk Grigoriy <31949421+Grigoriym@users.noreply.github.com>
2024-04-19 23:34:24 +0200

    Prepare for release/1.0-2 (#89)
    * Prepare for release/1.0-2
    
    * add release.yml
    
    * check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.grappim.hateitorrateit"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
