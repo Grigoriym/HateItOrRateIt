ed6fa5760b64ddb59e7d5bc0ff5ac90a2c7d6519
Mikhalchuk Grigoriy <31949421+Grigoriym@users.noreply.github.com>
2024-04-20 11:05:21 +0200

    v1.release/v1.0-3 (#94)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.grappim.hateitorrateit"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
