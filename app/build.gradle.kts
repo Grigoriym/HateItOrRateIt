9013d48f388a3c33ba2065fa0befb699cbfbdbfa
gregory <grigoriymikhalchuk@yahoo.com>
2025-02-22 00:09:29 +0100

    prepare release 1.1-9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
