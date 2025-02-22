d3679d0cfa7a20745810ced6d078d712c4a0fc7c
gregory <grigoriymikhalchuk@yahoo.com>
2025-02-22 10:04:27 +0100

    Prepare release v1.2-0011

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 10
-        versionName = "1.1"
+        versionCode = 11
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
