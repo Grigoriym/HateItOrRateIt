8cb206ffb005cb8ee9c037173d0a00e98a0891ca
gregory <grigoriymikhalchuk@yahoo.com>
2025-03-07 16:01:12 +0100

    clear imports

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -2,7 +2,6 @@ import com.android.build.api.dsl.ApplicationExtension
 import com.grappim.hateitorrateit.configureFlavors
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
